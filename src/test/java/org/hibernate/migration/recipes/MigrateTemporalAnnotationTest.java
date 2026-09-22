373f95614e6afd019acd36dc0fe3fd720074d3bd
Yoann Rodière <yoann@hibernate.org>
2026-09-22 08:48:58 +0000

    Address review feedback on PR #1
    - Rename package from org.hibernate.openrewrite to
      org.hibernate.migration.recipes to match the repo/artifact name
    - Update group ID to org.hibernate.migration
    - Set rootProject.name to hibernate-migration-recipes
    - Change rewrite-java-21 (testRuntimeOnly) to rewrite-java-17
      (runtimeOnly) since the project targets JDK 17+
    - Update all recipe names in jpa4.yml to use the new package
    - Add placeholder Hibernate ORM 7-to-8 migration recipe (orm8.yml)
      that wraps the JPA 3-to-4 recipe

--- src/test/java/org/hibernate/migration/recipes/MigrateTemporalAnnotationTest.java
+++ src/test/java/org/hibernate/migration/recipes/MigrateTemporalAnnotationTest.java
@@ -2,7 +2,7 @@
  * SPDX-License-Identifier: Apache-2.0
  * Copyright Red Hat Inc. and Hibernate Authors
  */
-package org.hibernate.openrewrite;
+package org.hibernate.migration.recipes;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
